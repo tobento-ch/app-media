c8fa5707302d8de13f7553b8f4a899afa4b7d771
TOBENTOch <info@tobento.ch>
2025-10-03 17:33:38 +0200

    message fixed as changed

--- tests/FileStorage/Writer/SvgSanitizerWriterTest.php
+++ tests/FileStorage/Writer/SvgSanitizerWriterTest.php
@@ -92,6 +92,6 @@ class SvgSanitizerWriterTest extends TestCase
             originalFilename: 'orgfilename.svg',
         );
         
-        $this->assertTrue($testHandler->hasRecord('SVG has sanitizing issues for the file :path.', Level::Info));
+        $this->assertTrue($testHandler->hasRecord('SVG has sanitizing issues for the file file.svg.', Level::Info));
     }    
 }
